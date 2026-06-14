5e971d9aebc1adacb65ce5358ec8f08a376a2189
Alejandro <alejandro.arancibia.aranda@gmail.com>
2026-06-14 02:26:56 -0400

    ci: add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alejaranda.github.io",
+  base: "/pokedex",
   vite: {
     plugins: [tailwindcss()],
   },
